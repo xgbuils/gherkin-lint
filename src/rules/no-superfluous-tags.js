0ea6f332d4fdfcc513dc41496515f036f54cfcad
Xavier Garcia Buils <xgbuils@gmail.com>
2019-05-12 08:29:57 +0200

    fix no-superfluous-tags: report superfluous example tags errors

--- src/rules/no-superfluous-tags.js
+++ src/rules/no-superfluous-tags.js
@@ -1,28 +1,44 @@
 const rule = 'no-superfluous-tags';
-const {compose, intoArray} = require('../utils/generic');
-const {filter, flatMap, map} = require('../utils/transducers');
-const {getFeatureNodes} = require('../utils/selectors');
-const {filterScenarios} = require('../utils/gherkin');
+const {applyOver, compose, flatMap} = require('../utils/generic');
+const {getExamples, getScenarios} = require('../utils/selectors');
 
 const getTagName = ({name}) => name;
 
-const createError = (tag) => {
-  return {
+const typesMap = {
+  Feature: 'feature',
+  ScenarioOutline: 'scenario',
+};
+
+const getNodeTagNames = ({tags, type}) => ({
+  names: new Set(tags.map(getTagName)),
+  type: typesMap[type],
+});
+
+const createErrors = (usedTags) => (tag) => {
+  const {type} = usedTags.find(({names}) => names.has(tag.name)) || {};
+  return type ? [{
     type: 'rule',
-    message: `${tag.name} tag is already used at Feature level`,
+    message: `${tag.name} tag is already used at ${type} level`,
     rule: rule,
     location: tag.location,
-  };
+  }] : [];
+};
+
+const checkSuperfluousTags = (usedTags) => (node) => {
+  return flatMap(createErrors(usedTags))(node.tags);
+};
+
+const noSuperfluousScenarioTags = (featureTags) => (scenario) => {
+  const usedTags = [featureTags].concat(getNodeTagNames(scenario));
+  return applyOver([
+    checkSuperfluousTags([featureTags]),
+    compose(flatMap(checkSuperfluousTags(usedTags)), getExamples),
+  ])(scenario);
 };
 
 const noSuperfluousTags = (feature) => {
-  const featureTags = new Set(feature.tags.map(getTagName));
-  return intoArray(compose(
-    filterScenarios,
-    flatMap(({tags}) => tags),
-    filter(({name}) => featureTags.has(name)),
-    map(createError)
-  ))(getFeatureNodes(feature));
+  const featureTags = getNodeTagNames(feature);
+  return flatMap(noSuperfluousScenarioTags(featureTags))(getScenarios(feature));
 };
 
 module.exports = {
