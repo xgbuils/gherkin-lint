eb02e1362cf351db57cbc8b5b0017977ae3961f7
Xavier Garcia Buils <xgbuils@gmail.com>
2019-05-15 20:56:28 +0200

    fix e2e tests package.json config

--- e2e-tests/compare.js
+++ e2e-tests/compare.js
@@ -1,25 +1,30 @@
 const fs = require('fs');
 const {promisify} = require('util');
-
 const readFile = promisify(fs.readFile);
 
-const [, , file1, file2] = process.argv;
+const compare = (actualFile, expectedFile) => {
+  return Promise.all([
+    readFile(actualFile, 'utf8'),
+    readFile(expectedFile, 'utf8'),
+  ])
+    .then(([actual, expected]) => {
+      return actual !== expected ? {
+        message: `actual:\n${actual}\n\nexpected:\n${expected}\n`,
+      } : null;
+    })
+    .catch((err) => ({
+      message: err,
+    }));
+};
 
 Promise.all([
-    readFile(file1, 'utf8'),
-    readFile(file2, 'utf8')
+  compare('stdout.txt', 'expected-stdout.txt'),
+  compare('stderr.txt', 'expected-stderr.txt'),
 ])
-.then(([actual, expected]) => {
-    const error = actual !== expected;
-    if (error) {
-        console.log('actual:')
-        console.log(actual);
-        console.log('expected:');
-        console.log(expected);
-    }
-    process.exit(error ? 1 : 0)
-})
-.catch((err) => {
-    console.error(err);
-    process.exit(1);
-});
+  .then((results) => {
+    const errors = results.filter((result) => result);
+    errors.forEach((error) => {
+      console.log(error.message); // eslint-disable-line no-console
+    });
+    process.exit(errors.length > 0 ? 1 : 0);
+  });
